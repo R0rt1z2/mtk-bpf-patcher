4e65fb8e4ac00e0022db564d6a7d3c0327e46c98
R0rt1z2 <me@r0rt1z2.com>
2023-04-21 09:27:51 +0200

    mtk-bpf-patcher: fix absolute paths for Windows
    * The python tool was failing to open the input and output file(s) because
      the wrapper executed a directory change BEFORE launching the python tool.
    
    * To fix this, save the original path and concatenate it to the first two
      arguments because we knows they're always the input / output file(s).

--- mtk_bpf_patcher/mtk-bpf-patcher.bat
+++ mtk_bpf_patcher/mtk-bpf-patcher.bat
@@ -1,14 +1,33 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-setlocal
 chcp 65001 2>nul >nul
+
+:: Save the original current path
+set "original_path=%CD%"
 
 :: Location should be where the main.py file is located
 :: This will be automatically updated via Install script
 cd "C:\Program Files\Python*\Scripts\mtk_bpf_patcher\mtk_bpf_patcher" 2>nul >nul
 
 if exist main.py (
-    python main.py %*
+    set "abs_args="
+    set "arg_count=0"
+    :arg_loop
+    if not "%~1"=="" (
+        set /A "arg_count+=1"
+        if !arg_count! LEQ 2 (
+            for %%A in ("%original_path%\%~1") do set "abs_arg=%%~fA"
+        ) else (
+            set "abs_arg=%~1"
+        )
+        set "abs_args=!abs_args! "!abs_arg!""
+        shift
+        goto arg_loop
+    )
+    python main.py !abs_args!
+	pause
+	exit
 ) else (
     powershell write-host -fore Red mtk-bpf-patcher not found!
 )
